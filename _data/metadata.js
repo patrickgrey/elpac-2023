58ccdcf2984740e6248c2f53474cad421434341d
patrickgrey <websemantic@yahoo.co.uk>
2023-09-27 16:27:50 +0100

    Updated test url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "ELPAC 2023",
-  url: "https://elpac.eurocontrol.int/",
+  url: "https://awake-bowl.cloudvent.net/",
   language: "en",
   description: "ELPAC | ICAO Language Proficiency Testing for air traffic controllers and pilots.",
   author: {
